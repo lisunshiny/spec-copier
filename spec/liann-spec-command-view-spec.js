41070047b097f621009860cacf0b5e899239367b
Liann Sun <lisunshiny@gmail.com>
2017-01-20 16:08:30 -0500

    initial commit

---
+++ spec/liann-spec-command-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LiannSpecCommandView from '../lib/liann-spec-command-view';
+
+describe('LiannSpecCommandView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
