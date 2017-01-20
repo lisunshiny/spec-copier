99e9d744519dcb810c5fc3af70118b779e51a08f
Liann Sun <lisunshiny@gmail.com>
2017-01-20 17:27:23 -0500

    remove more crufT

--- spec/liann-spec-command-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LiannSpecCommandView from '../lib/liann-spec-command-view';
-
-describe('LiannSpecCommandView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
